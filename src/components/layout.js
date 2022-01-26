ca8b5f3520ad00cc9b0940154765e1304dd5db90
Justin Anderson <justinandersun@gmail.com>
2022-01-25 21:20:15 -0800

    Designing header and footers, loading fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import {
   container,
-  heading,
+  header,
+  siteTitle,
+  leftHeader,
+  rightHeader,
+  headerImg,
   navLinks,
   navLinkItem,
   navLinkText,
-  siteTitle,
+  footer,
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
@@ -23,30 +28,40 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/guides" className={navLinkText}>
-              Guides
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={header}>
+        <div className={leftHeader}>
+          <StaticImage className={headerImg}
+            alt="Highpoint Guide Logo"
+            src="../images/logo.png"
+          />
+          <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+        </div>
+        <nav className={rightHeader}>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                Map
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/guides" className={navLinkText}>
+                Guides
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/about" className={navLinkText}>
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <main>
-        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
+      <footer className={footer}>
+        <p>Made and maintained with ❤️ by <a href="https://andersun.com" target="_blank" rel="noreferrer">Justin Andersun</a>.</p>
+      </footer>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home">
+    <Layout pageTitle="Map">
       <p>An interactive map and guidebook for climbing the 50 U.S. Highpoints</p>
-      <StaticImage
-        alt="Highpoint Logo"
-        src="../images/ushplogo.jpg"
-      />
     </Layout>
   )
 }
