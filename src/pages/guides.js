c731f6204628376d2ad5096d8bdba0b5cf23b67b
Justin Anderson <justinandersun@gmail.com>
2022-01-31 00:02:24 -0800

    Detail page template

--- src/pages/guides.js
+++ src/pages/guides.js
@@ -23,7 +23,6 @@ const GuidesPage = ({ data }) => {
         ))
       }
       </div>
-      <p>Cool!</p>
     </Layout>
   )
 }
--- src/pages/{mdx.slug}.js
+++ src/pages/{mdx.slug}.js
@@ -1,27 +1,81 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import {
   guidePage,
   guideTitle,
   guideState,
+  guideMasthead,
+  mastheadImage,
+  mastheadMetadata,
   guideMetadata,
+  metadataCell,
+  cellName,
+  cellData,
   guideBody,
 } from '../components/guide.module.css'
 
 const GuidePost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hp_image)
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.state}>
       <div className={guidePage}>
         <p className={guideTitle}>{data.mdx.frontmatter.highpoint}</p>
         <p className={guideState}>{data.mdx.frontmatter.state}</p>
-        <div className={guideMetadata}>
-          <p>Rank: {data.mdx.frontmatter.rank_elevation}</p>
-          <p>Elevation: {data.mdx.frontmatter.elevation_feet} feet / {data.mdx.frontmatter.elevation_meters} meters</p>
-          <p>Lat/Long: {data.mdx.frontmatter.lat} / {data.mdx.frontmatter.long}</p>
-          <p>Difficulty: {data.mdx.frontmatter.difficulty}</p>
-          <p>Season: {data.mdx.frontmatter.season}</p>
+        <div className={guideMasthead}>
+          <div className={mastheadImage}>
+            <GatsbyImage
+              image={image}
+              alt={data.mdx.frontmatter.hp_image_alt}
+            />
+          </div>
+          <div className={mastheadMetadata}>
+            <div className={guideMetadata}>
+            <div className={metadataCell}>
+              <p className={cellName}>Elevation</p>
+              <p className={cellData}>{data.mdx.frontmatter.elevation_feet} feet</p>
+            </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Location</p>
+                <p className={cellData}>{data.mdx.frontmatter.location}</p>
+              </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Elevation Rank</p>
+                <p className={cellData}>{data.mdx.frontmatter.rank_elevation} of 50</p>
+              </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Coordinates</p>
+                <p className={cellData}>{data.mdx.frontmatter.lat} / {data.mdx.frontmatter.long}</p>
+              </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Martin Difficulty Rating</p>
+                <p className={cellData}>{data.mdx.frontmatter.difficulty}</p>
+              </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Terrain</p>
+                <p className={cellData}>{data.mdx.frontmatter.terrain}</p>
+              </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Route Distance</p>
+                <p className={cellData}>{data.mdx.frontmatter.distance_miles} miles</p>
+              </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Vertical Gain</p>
+                <p className={cellData}>{data.mdx.frontmatter.gain_feet} feet</p>
+              </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Route Type</p>
+                <p className={cellData}>{data.mdx.frontmatter.route_type}</p>
+              </div>
+              <div className={metadataCell}>
+                <p className={cellName}>Climbing Season</p>
+                <p className={cellData}>{data.mdx.frontmatter.season}</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className={guideBody}>
           <MDXRenderer>
@@ -39,13 +93,23 @@ export const query = graphql`
       frontmatter {
         state
         highpoint
-        rank_elevation
-        elevation_feet
-        elevation_meters
+        location
         lat
         long
+        rank_elevation
+        elevation_feet
         difficulty
+        terrain
+        distance_miles
+        gain_feet
+        route_type
         season
+        hp_image_alt
+        hp_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
